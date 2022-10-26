08bef2e51d64271ae5f5e16ecedb5b7d01591bd4
Ahmed Sakr <80043395+Ahmed-Mohmed-Sakr@users.noreply.github.com>
2022-10-26 19:32:08 +0200

    adding search filter and put backboon of render

--- todo-app.js
+++ todo-app.js
@@ -6,16 +6,47 @@ const todos = [
   { text: "learn javascript", completed: false },
 ];
 
-const incompleteTodos = todos.filter(function (todo) {
-  return !todo.completed;
+const filters = {
+  searchtext: "",
+};
+
+const renderTodos = function (todos, filters) {
+  const filterTodos = todos.filter(function (todo) {
+    return todo.text.toLowerCase().includes(filters.searchtext.toLowerCase());
+  });
+
+  document.querySelector("#todos").innerHTML = "";
+
+  const incompleteTodos = filterTodos.filter(function (todo) {
+    return !todo.completed;
+  });
+
+  const summary = document.createElement("h2");
+  summary.textContent = `You have ${incompleteTodos.length} todos left`;
+  document.querySelector("#todos").appendChild(summary);
+
+  filterTodos.forEach(function (todo) {
+    const newP = document.createElement("p");
+    newP.textContent = todo.text;
+    document.querySelector("#todos").appendChild(newP);
+  });
+};
+
+renderTodos(todos, filters);
+
+// listen to new todo creation
+document.querySelector("#add-note").addEventListener("click", function (e) {
+  console.log("add a new todo");
+  // e.target.textContent = "yeah";
 });
 
-const summary = document.createElement("h2");
-summary.textContent = `You have ${incompleteTodos.length} todos left`;
-document.querySelector("body").appendChild(summary);
+document
+  .querySelector("#new-todo-text")
+  .addEventListener("input", function (e) {
+    console.log(e.target.value);
+  });
 
-todos.forEach(function (todo) {
-  const newP = document.createElement("p");
-  newP.textContent = todo.text;
-  document.querySelector("body").appendChild(newP);
+document.querySelector("#search-text").addEventListener("input", function (e) {
+  filters.searchtext = e.target.value;
+  renderTodos(todos, filters);
 });
